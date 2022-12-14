0566723cbdf7f9b0b1ff3d5bdbe8d3c210839d38
thidinhxm <thidinhxm@gmail.com>
2022-12-14 20:59:13 +0700

    Add import slide

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,4 +3,5 @@ module.exports.presentationController = require('./presentation.controller');
 module.exports.userController = require('./user.controller');
 module.exports.groupController = require('./group.controller');
 module.exports.participantController = require('./participant.controller');
-module.exports.answersController = require('./answer.controller');
+module.exports.answerController = require('./answer.controller');
+module.exports.slideController = require('./slide.controller');
